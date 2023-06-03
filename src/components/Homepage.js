a61e11b99124f3985e310b07db2aba17cf167e6a
Vishal <vishal.pugazhendhi@gmail.com>
2023-06-03 20:41:11 +0530

    draggable locations

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,27 +1,25 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, ZoomControl, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import L from 'leaflet';
 import 'leaflet-routing-machine';
 
 function Homepage() {
-    useEffect(() => {
-        const map = L.map('mapid').setView([47.6097, -122.3331], 13);
+    const seattleLatLong = [47.609687, -122.333131]; // Seattle coordinates
+    const newYorkLatLong = [47.609687, -122.20]; // New York coordinates
+
+    const routingUrl = `https://api.jawg.io/routing/route/v1/car/${seattleLatLong[1]},${seattleLatLong[0]};${newYorkLatLong[1]},${newYorkLatLong[0]}?overview=false&access-token=D7N8aZgZjVj8nE4w8TF0Onz6DLdRKWQefLhiXFzdAMvgHR5R6YkdQmz8xR50y81d`;
+
+    function handleMapLoad(map) {
         L.tileLayer(
             'https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=D7N8aZgZjVj8nE4w8TF0Onz6DLdRKWQefLhiXFzdAMvgHR5R6YkdQmz8xR50y81d',
             {}
         ).addTo(map);
 
-        const seattleLatLong = [47.609687, -122.333131]; // Seattle coordinates
-        const newYorkLatLong = [40.712237, -74.005742]; // New York coordinates
-
-        const routingUrl = `https://api.jawg.io/routing/route/v1/car/${seattleLatLong[1]},${seattleLatLong[0]};${newYorkLatLong[1]},${newYorkLatLong[0]}?overview=false&access-token=D7N8aZgZjVj8nE4w8TF0Onz6DLdRKWQefLhiXFzdAMvgHR5R6YkdQmz8xR50y81d`;
-
         fetch(routingUrl)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data); // Log the response data to check its contents
                 const { routes, waypoints } = data;
                 if (routes && routes.length > 0 && waypoints && waypoints.length > 1) {
                     const locations = waypoints.map((waypoint) => {
@@ -36,27 +34,35 @@ function Homepage() {
                         L.Routing.control({
                             waypoints: locations,
                             routeWhileDragging: true,
-                            show: true, // Set show to true to display the route on the map
+                            show: true,
                         }).addTo(map);
                     } else {
-                        console.log('Invalid waypoints'); // Log a message if the waypoints are invalid
+                        console.log('Invalid waypoints');
                     }
                 } else {
-                    console.log('No routes or waypoints found'); // Log a message if no routes or waypoints are found
+                    console.log('No routes or waypoints found');
                 }
             })
             .catch((error) => {
-                console.log('Error:', error); // Log any errors that occur during the fetch request
+                console.log('Error:', error);
             });
+    }
 
-        return () => {
-            map.remove();
-        };
-    }, []);
+    function MapComponent() {
+        const map = useMap();
+        useEffect(() => {
+            handleMapLoad(map);
+        }, [routingUrl]); // Only re-run the effect when routingUrl changes
+
+        return null;
+    }
 
     return (
         <div>
-            <div id="mapid"></div>
+            <MapContainer id="mapid" center={[47.6097, -122.3331]} zoom={13}>
+                <TileLayer url="https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=D7N8aZgZjVj8nE4w8TF0Onz6DLdRKWQefLhiXFzdAMvgHR5R6YkdQmz8xR50y81d" />
+                <MapComponent />
+            </MapContainer>
         </div>
     );
 }
